619dd6ad9913e771560c2e72696feb52677ee2d8
Nidhi-kumari <nidhi81194@gmail.com>
2019-02-12 01:02:51 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import nn
 import bayes
 import MNB
 import argparse
-import lsh
+#import lsh
 from sys import argv
 import warnings
 warnings.filterwarnings("ignore")
@@ -18,18 +18,18 @@ if __name__ == '__main__':
        run.predict_dolphins(test_data,test_label)
        nn.predict("../data/dolphins/dolphins.csv","../data/dolphins/dolphins_label.csv",test_data,test_label)
        bayes.predictNB("../data/dolphins/dolphins.csv","../data/dolphins/dolphins_label.csv",test_data,test_label)
-       lsh.run("dolphins")
+       #lsh.run("dolphins")
    elif data == "twitter":
        MNB.predict(test_data,test_label)
        #run.predict_twitter(test_data,test_label)
-       lsh.run("twitter")
+       #lsh.run("twitter")
        nn.predict_twitter(test_data,test_label)
        
        
    elif data == "pubmed":
        
        bayes.predictNB("../data/pubmed/pubmed.csv","../data/pubmed/pubmed_label.csv",test_data,test_label)
-       lsh.run("pubmed")
+       #lsh.run("pubmed")
        run.predict_pubmed(test_data,test_label)
        nn.predict("../data/pubmed/pubmed.csv","../data/pubmed/pubmed_label.csv",test_data,test_label)
        
